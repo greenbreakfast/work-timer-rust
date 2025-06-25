d193eb13bc649a92a8da765271a9b4f7bdc16323
Lazar <lazardemin@gmail.com>
2025-06-25 10:40:47 -0400

    added readme and gitignore, updated timer to display neater time remaining and % timer complete

---
+++ README.md
@@ -0,0 +1,17 @@
+# work-timer-rust
+
+Pomodoro timer in the command line. Defaults to 25 minutes.
+
+## Next steps
+
+- [x] update time remaining display with better formatting and percentage complete
+- [ ] end of timer should ding terminal bell
+- [ ] update time remaining display with a "loading bar"
+- [ ] write up compile + install instructions in readme
+- [ ] add optional command-line arg to change 
+
+## How to run in development
+
+```
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,19 @@ use crossterm::{
     ExecutableCommand,
 };
 
-fn format_remaining_timer(timer: i32) -> String {
-    let mut s = String::new();
-    let mut min;
-    let mut seconds = timer;
-    if timer > 60 {
-        seconds = timer % 60;
-        min = (timer - seconds)/60;
- 
-        s = format!("{min} minutes, ");
-    } 
-    s = format!("{s}{seconds} seconds");
+fn format_remaining_time(timer: i32) -> String {
+    let seconds = timer % 60;
+    let min = (timer - seconds)/60;
+
+    let s = format!("{min}m{seconds}s");
     return s;
 }
 
-fn display_remaining_timer(timer: i32) {
+fn percent_complete(timer: i32, timer_start_val: i32) -> String {
+    return format!("{:.0}%", ((timer_start_val - timer) as f32/timer_start_val as f32)*100.0);
+}
+
+fn display_remaining_timer(timer: i32, timer_start_val: i32) {
     let mut stdout = io::stdout();
     
     // Clear the current line and move cursor to beginning
@@ -30,25 +28,26 @@ fn display_remaining_timer(timer: i32) {
         .unwrap();
     
     // Print the timer without a newline
-    print!("\tRemaining: {}", format_remaining_timer(timer));
+    print!("\tRemaining: {}\t{}", format_remaining_time(timer), percent_complete(timer, timer_start_val));
     
     // Flush to ensure immediate display
     stdout.flush().unwrap();
 }
 
 fn main() {
-    let mut timer = 25*60;
-    println!("Timer for {} seconds", timer);
+    let timer_start_val = 25*60;
+    let mut timer = timer_start_val;
+    println!("Timer for {}", format_remaining_time(timer));
     
     // Print initial timer display
-    display_remaining_timer(timer);
+    display_remaining_timer(timer, timer_start_val);
     
     loop {
         thread::sleep(Duration::from_millis(1000));
         timer -= 1;
 
         // update display
-        display_remaining_timer(timer);
+        display_remaining_timer(timer, timer_start_val);
 
         if timer == 0 {
             break;
